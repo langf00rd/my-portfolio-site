0aad3fe38ee39c432bab8caf5d5dcd0e6290fb32
langford <langfordquarshie21@gmail.com>
2023-10-23 16:35:55 +0000

    add istream project

--- components/ScrollButtons/index.tsx
+++ components/ScrollButtons/index.tsx
@@ -1,4 +1,4 @@
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import { ReactNode } from 'react';
 import { FiArrowUp, FiArrowDown } from 'react-icons/fi';
 import useScroll from '~/hooks/useScroll.hook';
@@ -7,16 +7,23 @@ export default function ScrollButtons(): JSX.Element {
   const { scrollPosition, scrollDown, scrollUp } = useScroll();
   return (
     <>
-      {scrollPosition && scrollPosition.y > 100 && (
-        <div className='fixed bottom-[30px] right-[30px] grid gap-3'>
-          <FAB onClick={scrollUp}>
-            <FiArrowUp />
-          </FAB>
-          <FAB onClick={scrollDown}>
-            <FiArrowDown />
-          </FAB>
-        </div>
-      )}
+      <AnimatePresence>
+        {scrollPosition && scrollPosition.y > 100 && (
+          <motion.div
+            initial={{ opacity: 0, x: 40 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: 40 }}
+            className='fixed bottom-[30px] right-[30px] grid gap-3'
+          >
+            <FAB onClick={scrollUp}>
+              <FiArrowUp />
+            </FAB>
+            <FAB onClick={scrollDown}>
+              <FiArrowDown />
+            </FAB>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   );
 }
@@ -25,8 +32,6 @@ export function FAB(props: { children: ReactNode; onClick?: () => void }): JSX.E
   return (
     <motion.div
       onClick={props.onClick}
-      initial={{ opacity: 0, x: 30 }}
-      animate={{ opacity: 1, x: 0 }}
       className='shadow-md hidden hover:shadow-none active:scale-[0.8] border rounded-full w-[45px] h-[45px] md:flex items-center justify-center cursor-pointer hover:text-brand z-10 bg-white'
     >
       {props.children}
